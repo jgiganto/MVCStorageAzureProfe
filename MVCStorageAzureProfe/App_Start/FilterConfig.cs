fdb3b10efb966592fd3d68c253bee46a74cd117e
jgiganto <jgiganto1@hotmail.com>
2018-04-13 09:16:14 +0200

    Add project files.

---
+++ MVCStorageAzureProfe/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStorageAzureProfe
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
